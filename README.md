dccf095962645d7b5f5ba5833138f386d52ee2cc
Munizzs <matheusmuniz0810@gmail.com>
2021-08-03 15:32:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# DeBairro_TCC" 
---
+++ app/src/main/java/com/example/debairro_tcc/Tela_Login_e_Cadastrar.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Login_e_Cadastrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela__login_e__cadastrar);
+    }
+}
