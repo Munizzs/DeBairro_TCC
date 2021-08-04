e5c782732785db4d87d3c3b26bd91f9ffba91cf3
Munizzs <matheusmuniz0810@gmail.com>
2021-08-04 00:47:59 -0300

    Tela de login feita

---
+++ app/src/main/java/com/example/debairro_tcc/Tela_de_Login.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_de_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de__login);
+    }
+}
