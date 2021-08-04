8405a1dab02f5478f3784c03a7994aec46d68544
Munizzs <matheusmuniz0810@gmail.com>
2021-08-04 14:00:25 -0300

    Tela de Cadastro 2

---
+++ app/src/main/java/com/example/debairro_tcc/Tela_de_Cadastro2.java
@@ -0,0 +1,25 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Tela_de_Cadastro2 extends AppCompatActivity {
+
+    Spinner sexo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de__cadastro2);
+
+        sexo = (Spinner) findViewById(R.id.SPN_Sexo);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Sexo, android.R.layout.simple_spinner_item);
+        sexo.setAdapter(adapter);
+
+
+    }
+}
