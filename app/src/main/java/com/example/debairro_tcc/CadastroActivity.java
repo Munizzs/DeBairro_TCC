9e0882af5c0260058ff38c50d41159533f24e04b
menezessn <mar_chelo10@hotmail.com>
2021-08-08 01:05:40 -0300

    Atualização na tela de cadastro e na tela inicial

--- app/src/main/java/com/example/debairro_tcc/CadastroActivity.java
+++ app/src/main/java/com/example/debairro_tcc/CadastroActivity.java
@@ -2,9 +2,13 @@ package com.example.debairro_tcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
+    ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,18 @@ public class CadastroActivity extends AppCompatActivity {
         setTheme(R.style.Theme_DeBairro_TCC);
         setContentView(R.layout.cadastro_layout);
         getSupportActionBar().hide();
+        mViewHolder.btnProximo = findViewById(R.id.btn_proximo);
+        mViewHolder.btnProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CadastroActivity.this, Cadastro2Activity.class);
+                startActivity(i);
+            }
+        });
 
+    }
+
+    public static class ViewHolder {
+        Button btnProximo;
     }
 }
