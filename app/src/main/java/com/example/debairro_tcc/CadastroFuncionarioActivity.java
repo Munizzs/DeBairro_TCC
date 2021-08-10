faf5d5565dc8b98137364462a150ab544d0cb929
Munizzs <matheusmuniz0810@gmail.com>
2021-08-10 17:41:17 -0300

    Atualização: - Tela "nova_venda" (Não terminado)

--- app/src/main/java/com/example/debairro_tcc/CadastroFuncionarioActivity.java
+++ app/src/main/java/com/example/debairro_tcc/CadastroFuncionarioActivity.java
@@ -10,5 +10,6 @@ public class CadastroFuncionarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_funcionario_layout);
+        getSupportActionBar().hide();
     }
 }
---
+++ app/src/main/java/com/example/debairro_tcc/NovaVendaLayoutActivity.java
@@ -0,0 +1,37 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+
+public class NovaVendaLayoutActivity extends AppCompatActivity {
+
+    ArrayList<String> FormaDePagamento;
+    ArrayAdapter<String> FormaPagamento;
+    CadastroAdministrador.ViewHolder mViewHolder = new CadastroAdministrador.ViewHolder();
+    String formaPagamento[];
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        formaPagamento = getResources().getStringArray(R.array.FormaDePagamento);
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_item, formaPagamento);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nova_venda_layout);
+
+
+    }
+}
