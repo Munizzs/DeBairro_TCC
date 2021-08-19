d531b7634c9362b3473a9968d80c505d156283fa
menezessn <mar_chelo10@hotmail.com>
2021-08-18 22:41:39 -0300

    Inicio da implementação da top bar

--- app/src/main/java/com/example/debairro_tcc/ClientesFragment.java
+++ app/src/main/java/com/example/debairro_tcc/ClientesFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ClientesFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,6 +55,7 @@ public class ClientesFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -61,4 +64,5 @@ public class ClientesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_clientes_layout, container, false);
     }
+
 }
