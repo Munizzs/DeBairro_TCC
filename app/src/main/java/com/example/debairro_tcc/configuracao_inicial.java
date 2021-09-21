b03a42f51aeb3d499f66f81c63138e8022105720
LucasPatrickF <lucasokanalma@gmail.com>
2021-09-21 15:14:15 -0300

    criação da tela de configuração

---
+++ app/src/main/java/com/example/debairro_tcc/configuracao_inicial.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class configuracao_inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuracao_inicial);
+    }
+}
