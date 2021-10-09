727a622b8ac458ec0d00d9e26e2017ed3641430c
LucasPatrickF <lucasokanalma@gmail.com>
2021-10-09 15:19:31 -0300

    Criação da tela de adicionar maquininha

---
+++ app/src/main/java/com/example/debairro_tcc/Adicionar_maquininha.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Adicionar_maquininha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adicionar_maquininha_layout);
+    }
+}
