40aef7fc2a89b6d32ff54c61ec678106832589c4
LucasPatrickF <lucasokanalma@gmail.com>
2021-10-11 15:39:13 -0300

    Criação da tela de adicionar funcionario 2

---
+++ app/src/main/java/com/example/debairro_tcc/AdicionarFuncionario2Activity.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarFuncionario2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adicionar_funcionario2_layout);
+    }
+}
