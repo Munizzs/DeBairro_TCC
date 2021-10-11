6361b08a06d5b751c58af1c2bd906c81c65fda31
phlvss <paulolvs6@gmail.com>
2021-10-11 15:10:42 -0300

    Adição da tela informacoes_do_usuario_layout.xml

--- app/src/main/java/com/example/debairro_tcc/AdicionarMaquininhaActivity.java
+++ app/src/main/java/com/example/debairro_tcc/AdicionarMaquininhaActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Adicionar_maquininha extends AppCompatActivity {
+public class AdicionarMaquininhaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/debairro_tcc/InformacoesUsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformacoesUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.informacoes_do_usuario_layout);
+    }
+}
