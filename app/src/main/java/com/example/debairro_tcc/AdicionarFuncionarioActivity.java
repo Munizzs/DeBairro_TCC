e338236a9419354f3f186f9bf2b0f40899e35eb3
Munizzs <matheusmuniz0810@gmail.com>
2021-10-13 09:47:58 -0300

    Update         -Tela Adiciona Funcionario criada.

---
+++ app/src/main/java/com/example/debairro_tcc/AdicionarFuncionarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarFuncionarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adicionar_funcionario_layout);
+    }
+}
