ae14ed62d49692f9c272988a7f16dbcf813eaa17
LucasPatrickF <lucasokanalma@gmail.com>
2021-10-15 00:43:39 -0300

    Cração da tela sobre e correção de alguns erros - Lucas

---
+++ app/src/main/java/com/example/debairro_tcc/ConfiguracaoSobreActivity.java
@@ -0,0 +1,14 @@
+package com.example.debairro_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracaoSobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuracao_sobre_layout);
+    }
+}
