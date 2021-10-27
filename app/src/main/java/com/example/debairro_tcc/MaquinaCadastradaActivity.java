94e738279633a3c9dcf37d997c493c55cba6c169
LucasPatrickF <lucasokanalma@gmail.com>
2021-10-26 23:48:41 -0300

    top bar finalizada, alteração dos fragments, e correção do idioma

--- app/src/main/java/com/example/debairro_tcc/MaquinaCadastradaActivity.java
+++ app/src/main/java/com/example/debairro_tcc/MaquinaCadastradaActivity.java
@@ -2,6 +2,7 @@ package com.example.debairro_tcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,19 +10,27 @@ import android.widget.Button;
 
 public class MaquinaCadastradaActivity extends AppCompatActivity {
 
-    private Button addmaquina;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maquina_layout);
 
-        addmaquina = findViewById(R.id.btn_Maquininha);
+        Button addmaquina = findViewById(R.id.btn_Maquininha);
         addmaquina.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent intent = new Intent(MaquinaCadastradaActivity.this, AdicionarMaquininhaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button voltarmaquina = findViewById(R.id.btn_VoltarMaquina);
+        voltarmaquina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MaquinaCadastradaActivity.this, ConfiguracaoInicialActivity.class);
                 startActivity(intent);
             }
         });
